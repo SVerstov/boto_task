531d50114422db83c44034c9046b2785214f50fd
Seliverstov <seli@verstov.pro>
2024-02-07 16:38:01 +0400

    refactor: move velidators

--- endpoints/short_links.py
+++ endpoints/short_links.py
@@ -1,46 +1,16 @@
 # ruff: noqa: B008
-from typing import Any
 
 from fastapi import APIRouter, Depends, HTTPException, Request, status
 from fastapi.responses import JSONResponse, RedirectResponse, Response
 from loguru import logger
-from pydantic import BaseModel, HttpUrl, field_validator, model_validator
 
 from config import Config
 from db import ShortLink
 from db.dao import DAO
 from endpoints.utils import get_and_check_random_string, get_config, get_dao
+from endpoints.validators import LinkCreate, LinkUpdate
 
 router = APIRouter()
-
-
-class LinkCreate(BaseModel):
-  url: HttpUrl
-  status_code: int = 301
-
-  @field_validator("url")
-  def convert_url_to_str(cls, value):
-    if value:
-      return str(value)
-
-  @field_validator("status_code")
-  def validate_status_code(cls, value):
-    if isinstance(value, int) and (301 <= value <= 308):
-      return value
-    err_msg = "Invalid status code"
-    raise ValueError(err_msg)
-
-
-class LinkUpdate(LinkCreate):
-  url: HttpUrl | None = None
-  status_code: int | None = None
-
-  @model_validator(mode='before')
-  @classmethod
-  def has_at_least_1_param(cls, data: Any) -> Any:
-    assert isinstance(data, dict)
-    assert data.get("url") or data.get("status_code")
-    return data
 
 
 @router.post("/api/links/")
